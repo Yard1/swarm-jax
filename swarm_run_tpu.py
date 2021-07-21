3aae00970406654fc6af16d00c076e88132b004a
Antoni Baum <antoni.baum@protonmail.com>
2021-07-21 17:50:37 +0200

    Config

--- swarm_run_tpu.py
+++ swarm_run_tpu.py
@@ -3,46 +3,53 @@ import multiprocessing
 
 import optax
 import ray
+import traceback
 
 from loader import TextLoader
-from ray_tpu import start_ray, get_connection, create_tpu, wait_til
+from ray_tpu import start_ray, get_connection, create_tpu, wait_til, delete_tpu
 from swarm_jax.model import SwarmCharTransformerBig
 from swarm_jax.swarm import Swarm
 from swarm_jax.swarm_layer import NetworkPrecision
 
-tpus = 8
+if __name__ == '__main__':
+    tpus = 2
+    zone = "europe-west4-a"
 
-# for i in range(tpus):
-#     delete_tpu(f"swarm-jax-test-{i}", "europe-west4-a")
-#
-# exit()
+    # for i in range(tpus):
+    #     delete_tpu(f"swarm-jax-test-{i}", zone)
 
-head_info = ray.init(dashboard_host="0.0.0.0")
-address = head_info['redis_address']
+    # exit()
 
-conns = []
-for i in range(tpus):
-    create_tpu(f"swarm-jax-test-{i}", "europe-west4-a", "v3-8", False)
+    head_info = ray.init(dashboard_host="0.0.0.0")
+    address = head_info['redis_address']
 
-for i in range(tpus):
-    assert wait_til(f"swarm-jax-test-{i}", "europe-west4-a", {'state': 'READY', 'health': 'HEALTHY'})
+    conns = []
+    for i in range(tpus):
+        create_tpu(f"swarm-jax-test-{i}", zone, "v3-8", False)
 
-for i in range(tpus):
-    conns += get_connection(f"swarm-jax-test-{i}", "europe-west4-a")
+    try:
+        for i in range(tpus):
+            assert wait_til(f"swarm-jax-test-{i}", zone, {'state': 'READY', 'health': 'HEALTHY'})
 
-with multiprocessing.Pool(processes=tpus) as p:
-    p.map(functools.partial(start_ray, address=address), conns)
+        for i in range(tpus):
+            conns += get_connection(f"swarm-jax-test-{i}", zone)
 
-train_dataset = TextLoader("data/enwik9", batchsize=(8, 8), sample_size=1024, length=90000000)
+        with multiprocessing.Pool(processes=tpus) as p:
+            p.map(functools.partial(start_ray, address=address), conns)
 
-optimizer = optax.chain(
-    optax.clip_by_global_norm(0.25),
-    optax.adam(2e-4, b1=0.9, b2=0.99, eps=1e-5))
+        train_dataset = TextLoader("data/enwik8", batchsize=(8, 8), sample_size=1024, length=90000000)
 
-prec = NetworkPrecision(fwd_act="float32", rev_act="float32", grad="float32")
+        optimizer = optax.chain(
+            optax.clip_by_global_norm(0.25),
+            optax.adam(2e-4, b1=0.9, b2=0.99, eps=1e-5))
 
-model = SwarmCharTransformerBig
-swarm = Swarm(model, optimizer, 2 ** 16, train_dataset.get_samples, prec)
-swarm.run(100000, "runs/512_30L", "ckpt/512_30L")
+        prec = NetworkPrecision(fwd_act="float32", rev_act="float32", grad="float32")
 
-ray.shutdown()
+        model = SwarmCharTransformerBig
+        swarm = Swarm(model, optimizer, 2 ** 16, train_dataset.get_samples, prec)
+        swarm.run(100000, "runs/512_30L", "ckpt/512_30L")
+    except Exception:
+        traceback.print_exc()
+        for i in range(tpus):
+            delete_tpu(f"swarm-jax-test-{i}", zone)
+    ray.shutdown()
